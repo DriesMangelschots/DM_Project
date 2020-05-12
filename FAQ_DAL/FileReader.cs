f8d375d731a4e1a518144a60ede2e96eaa30cd2b
Matix072 <mathias.tweepenninckx@gmail.com>
2020-05-12 13:08:06 +0200

    First commit

---
+++ FAQ_DAL/FileReader.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+
+namespace FAQ_DAL
+{
+    public class FileReader
+    {
+
+    }
+}
